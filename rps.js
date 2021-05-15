e606d8dfacbd54e0176975463aedff7c52a1b330
Rajiv <rairajivwar@gmail.com>
2021-05-15 17:54:04 +0530

    Added UI

--- rps.js
+++ rps.js
@@ -5,70 +5,105 @@ function computerPlay()
 }
 function round(player,computer)
 {
-    var userInput = prompt("Enter Your Imput", "rock");
-    playerSelection = userInput.toLowerCase();
-    computerSelection = computerPlay();
-
-    if(playerSelection == computerSelection)
+    if(player == computer)
     {
-        console.log("The round is a tie");
-        return [player,computer];
+        return [0,0];
     }
-    else if (playerSelection == "rock")
+    else if (player == "rock")
     {
-        if (computerSelection == "paper")
+        if (computer == "paper")
         {
-            console.log("You lost this round");
-            return [player,computer+1];
+            return [0,1];
         }
         else
         {
-            console.log("You won this round");
-            return [player+1,computer];
+            return [1,0];
         }
     }
-    else if (playerSelection=="paper")
+    else if (player=="paper")
     {
-        if (computerSelection == "rock")
+        if (computer == "rock")
         {
-            console.log("You won this round");
-            return [player+1,computer];
+            return [1,0];
         }
         else
         {
-            console.log("You lost this round");
-            return [player,computer+1];
+            return [0,1];
         }
     }
-    else if (playerSelection=="scissors")
+    else if (player=="scissors")
     {
-        if (computerSelection == "rock")
+        if (computer == "rock")
         {
-            console.log("You lost this round");
-            return [player,computer+1];
+            return [0,1];
         }
         else
         {
-            console.log("You won this round");
-            return [player+1,computer];
+            return [1,0];
         }
     }
 }
-var player=0,computer=0; 
-for(i=0;i<=4;i++)
-{
-     [player,computer]=round(player,computer);
-}
-console.log("The max limit of rounds reached");
-if(computer>player)
-{
-    console.log("Computer Wins the Game");
-}
-else if(computer==player)
-{
-    console.log("The Game has been Tied");
-}
-else
-{
-    console.log("Player wins the game");
-}
+var count=1;
+playerScore=0,computerScore=0;
+const main = document.querySelector('.main');
+const div1 = document.createElement('div');
+div1.classList.add("div1");
+const btns = document.querySelectorAll('button');
+btns.forEach((button) => {
+    button.addEventListener('click',() =>{
+        if (count<6)
+        {
+            
+            var player = button.id;
+            var score1, score2;
+            var computer = computerPlay();
+            [score1,score2]= round(player,computer);
+            if (score1 == score2)
+            {
+                div1.style.cssText = "margin:50px; padding:50px,50px;text-align:center; color:white; border:solid; width:220px; height:20px;"
+                div1.textContent="Round "+ count +": Tied";
+            }
+            else if (score1 == 1)
+            {
+                div1.style.cssText = "margin:50px; padding:50px,50px;text-align:center; color:white; border:solid; width:220px; height:20px;"
+                div1.textContent="Round "+ count +": Player";
+            }
+            else
+            {
+                div1.style.cssText = "margin:50px; padding:50px,50px;text-align:center; color:white; border:solid; width:220px; height:20px;"
+                div1.textContent="Round "+ count +": Computer";
+            }
+            playerScore =playerScore + score1;
+            computerScore =computerScore + score2;
+            console.log(playerScore);  
+            count++;
+            if (count==6)
+            {
+                const d1 = document.querySelector('.main');
+                const res = document.createElement('div');
+                if(playerScore>computerScore)
+                {
+                    res.style.cssText= "margin:50px; padding:50px,50px;text-align:center; color:green; border:solid; width:220px; height:20px;"
+                    res.textContent="winner:Player";
+                }
+                else if(playerScore==computerScore)
+                {
+                    res.style.cssText = "margin:50px; padding:50px,50px;text-align:center; color:white; border:solid; width:220px; height:20px;"
+                    res.textContent="Game Tied";
+                }
+                else
+                {
+                    res.style.cssText = "margin:50px; padding:50px,50px;text-align:center; color:red; border:solid; width:220px; height:20px;"
+                    res.textContent="winner:Computer";    
+                }
+                d1.append(res);    
+            }  
+        }
+        else
+        {
+            div1.style.cssText="margin:50px;color:red; padding:50px,50px; text-align:center;width:220px; height:20px;";
+            div1.textContent="Maximum Limit Reached";
+        } 
+    });
+});
+main.append(div1);
